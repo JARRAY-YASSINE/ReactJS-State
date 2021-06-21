0daec67b133d449d219a055dace832f206975a07
Yassine JARRAY <yassine.jarray@enig.u-gabes.tn>
2021-06-21 16:28:41 +0500

    commit for state

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import {Button} from 'react-bootstrap';
+import './App.css';
+import yass from "./assets/yass.jpeg"
+
+
+class App extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			interval: null,
+			timer: 0
+		};
+	}
+
+	componentDidMount(){
+		this.setState ({
+			interval: setInterval( () =>{
+				this.setState({timer:this.state.timer + 1});
+			},1000)
+		})
+	}
+
+	componentWillUnmount(){
+		clearInterval(this.state.interval)
+	}
+
+    state={
+      isVisible:false
+    }
+
+    person=[
+      'Yass Yassine',
+      'Graduated in 2017',
+      'Embedded Software engineer'
+    ]
+  toggleVisibility = () => {
+    this.setState({
+      isVisible : !this.state.isVisible
+    })
+  }
+
+render(){
+  return (
+    <div className='page'>
+			<div className="mb-2">
+			<h2 className='timer'>{this.state.timer}</h2>	
+      <Button variant="primary" size="sm" onClick={this.toggleVisibility}>{this.state.isVisible ? "Hide profile" : "Show profile" }
+			</Button>{' '}
+			</div>
+      {this.state.isVisible ? 
+    (
+    <div>
+      <ul className='list'>
+       {this.person.map((info,i)=> (
+         <li key={i}>{info}</li>
+         ))}
+      </ul>   
+      <img className='img' src={yass} alt='img' width='400px'></img>
+			
+    </div>
+		
+    ): (<h4>Click on the button to see the profile</h4>)}
+    </div>
+  );
+}
+}
+export default App;
